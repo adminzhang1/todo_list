47a8838cbd3fc53320fa547ef859faf0fbdc35d4
adminzhang1 <a2324086024@163.com>
2022-10-10 20:45:15 +0800

    动画更新

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,8 @@
 <template>
 	<ul class="todo-main">
-		<ItemVue v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj"></ItemVue>
+		<transition-group name="todo" appear>
+			<ItemVue v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj"></ItemVue>
+		</transition-group>
 	</ul>
 </template>
 
@@ -30,5 +32,19 @@
 		border-radius: 2px;
 		padding-left: 5px;
 		margin-top: 10px;
+	}
+	.todo-enter-active{
+		animation: todoAnimat 0.5s linear;
+	}
+	.todo-leave-active{
+		animation: todoAnimat 0.5s linear reverse;
+	}
+	@keyframes todoAnimat{
+		from{
+			transform: translateX(-100%);
+		}
+		to{
+			transform: translateX(0px);
+		}
 	}
 </style>
